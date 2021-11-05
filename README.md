270bef1b615ac2bd07ef28bea3cb71e745e3f7c4
Jane Hung <janehung@berkeley.edu>
2021-11-05 03:04:32 +0000

    update bugs in data download

--- README.md
+++ README.md
@@ -11,7 +11,10 @@ Final Project for Fall 2021
 3. Open Docker container  
 ```docker run --gpus all -it --rm -p 8888:8888 --shm-size=2048m -v "$PWD":/data/ nvcr.io/nvidia/pytorch:21.09-py3```
 
-4. Run [setup.sh](setup.sh) which contains instructions for repo download.
+4. Run [../data/setup.sh](setup.sh) which contains instructions for repo download.
+
+5. Move to the right directory
+```cd ../data/pytorch-ssd```
 
 5. Retrain using these images  
 ```python train_ssd.py --dataset_type voc --datasets /data/helmet --net mb1-ssd --pretrained_ssd models/mobilenet-v1-ssd-mp-0_675.pth --scheduler cosine --lr 0.01 --t_max 100 --validation_epochs 5 --num_epochs 100 --base_net_lr 0.001  --batch_size 5```
--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
+# get into right directory
+cd ../data
+
 # jetson inference
 git clone https://github.com/dusty-nv/jetson-inference.git
 
 # opencv
-pip install opencv-python
+pip install -r requirements.txt
 
 # get pytorch-ssd repo
 git clone https://github.com/qfgaohao/pytorch-ssd.git
@@ -19,11 +22,8 @@ wget -P models https://storage.googleapis.com/models-hao/mb2-ssd-lite-mp-0_686.p
 wget -P models https://storage.googleapis.com/models-hao/vgg16-ssd-mp-0_7726.pth
 
 # get data
-sudo wget -P data/helmet https://w251-masknomask.s3.amazonaws.com/helmet.zip
-sudo unzip -q data/helmet/helmet.zip -d data/helmet
-sudo chmod go+rx data/helmet
-sudo chmod go+rx test
-sudo chmod go+rx train
+wget -P data/helmet https://w251-masknomask.s3.amazonaws.com/helmet.zip
+unzip -q data/helmet/helmet.zip -d data/helmet
 
 # for cv2 error --> ImportError: libGL.so.1: cannot open shared object file: No such file or directory
 apt-get update
